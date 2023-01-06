b96397e0f0f74a50bb6f0f94f9ae7756808b2de9
Ittetsu Hirata <science8613@gmail.com>
2023-01-06 16:23:59 +0900

    抽象構文木（加減算）の実装

--- 9cc.c
+++ 9cc.c
@@ -28,11 +28,39 @@ struct Token {
 */
 
 struct Node {
-	NodeKind kind;
-	Node *lhs;
-	Node *rhs;
-	int val;
+	NodeKind kind; // ノードの型
+	Node *lhs;     // 左辺	
+	Node *rhs;     // 右辺
+	int val;       // kindがND_NUMの場合のみ使う
 };
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for(;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
 
 //現在着目しているトークン
 Token *token;
