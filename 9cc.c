288f3ecc6b647049ebfe880b71004dd251761663
Ittetsu Hirata <science8613@gmail.com>
2023-01-06 13:31:16 +0900

    試験

--- 9cc.c
+++ 9cc.c
@@ -6,18 +6,32 @@
 #include <string.h>
 
 typedef enum {
-	TK_RESERVED, //記号
-	TK_NUM,	     //整数トークン
-	TK_EOF,	     //入力の終わりを示すトークン
-} TokenKind;
+//	TK_RESERVED, //記号
+//	TK_NUM,	     //整数トークン
+//	TK_EOF,	     //入力の終わりを示すトークン
+	ND_ADD, // +
+	ND_SUB,	// -
+	ND_MUL,	// *
+	ND_DIV,	// /
+	ND_NUM,	// 整数
+} NodeKind;
 
-typedef struct Token Token;
+typedef struct Node Node;
 
+/*
 struct Token {
 	TokenKind kind; //トークンの型
 	Token *next;	//次の入力トークン
 	int val;	//kindがTK_NUMの場合、その数値
 	char *str;	//トークン文字列
+};
+*/
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
 };
 
 //現在着目しているトークン
